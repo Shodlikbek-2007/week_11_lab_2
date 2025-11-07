e693add075a2e011dacdc96b856f11d2824022ac
Shodlikbek-2007 <shavkatovshodlikbek@gmail.com>
2025-11-07 10:29:46 +0500

    Clean up comments and formatting in problem 5.cpp

--- problem 5.cpp
+++ problem 5.cpp
@@ -6,28 +6,27 @@ int main() {
     int n;
     cin >> n;
     
-    vector<int> a, b, result;  // Empty vectors
+    vector<int> a, b, result; 
     
-    // Read first vector
+
     for (int i = 0; i < n; i++) {
         int num;
         cin >> num;
         a.push_back(num);
     }
     
-    // Read second vector  
+  
     for (int i = 0; i < n; i++) {
         int num;
         cin >> num;
         b.push_back(num);
     }
     
-    // Calculate sum
+
     for (int i = 0; i < n; i++) {
         result.push_back(a[i] + b[i]);
     }
     
-    // Output result
     for (int num : result) {
         cout << num << " ";
     }
